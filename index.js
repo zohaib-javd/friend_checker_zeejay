f7a72ba0ac2231521bdcf4c30b5a6dfea98e0b19
Zohaib Javed <159166949+zohaib-javd@users.noreply.github.com>
2024-05-18 20:05:42 +0500

    Friend Checker Game

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let isFriend = await inquirer.prompt([{
+        name: "name",
+        type: "string",
+        message: "Enter your friend name Zeejay:"
+    }]);
+if (isFriend.name === "Fahad" || isFriend.name === "Shahbaz") {
+    console.log(`${isFriend.name} is your friend.`);
+}
+else {
+    console.log(`${isFriend.name} is not your friend.`);
+}
